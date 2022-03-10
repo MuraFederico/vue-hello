23e1b619cebae1650ca3c802f9ddd82baf502325
MuraFederico <federicomura98@gmail.com>
2022-03-10 14:36:44 +0100

    esercizio completo

---
+++ script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+
+    el: '#app',
+    data: {
+      message: 'Hello World',
+      image: 'https://picsum.photos/200/300',
+      alt: 'immagine casuale'
+    },
+    
+  })
+
+
